39ea8cee9b8bbe7964cd856b24e7e4d51ae4f0be
Gaurav Singh <gauravkutlehria3@gmail.com>
2021-08-08 19:08:16 +0530

    day four buid

--- Documents/airbnb-yt/next.config.js
+++ Documents/airbnb-yt/next.config.js
@@ -2,4 +2,7 @@ module.exports={
     images: {
         domains: ['links.papareact.com'],
     },
+    env:{
+        mapbox_key: "pk.eyJ1IjoiZ2F1cmF2OTkiLCJhIjoiY2tzMmxndmE3MW5tNjJwbzloejJqNHplOSJ9.Iu3B_KCZL4uossfVvfnjEw",
+    }
 };
